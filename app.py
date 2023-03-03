823acb1e8297ccae6abb71b2aec2852b204b750f
Muhammad Fahad <116518111+Forged-Fahad@users.noreply.github.com>
2023-03-03 21:35:56 +0000

    Added a list of planets with flask basics

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+PLANETS = [
+  {
+    'id': 1,
+    "name" : "55 Canceri E",
+    "galaxy" : "Milky Way",
+    "distance" : "40 light years"
+  },
+  {
+    'id': 2,
+    "name" : "TOI 700",
+    "galaxy" : "Messier 51",
+    "distance" : "100 light years"
+  },
+  {
+    'id': 3,
+    "name" : "PA-99-N2",
+    "galaxy" : "Andromeda",
+    "distance" : "2,185,247 light years"
+  },
+  {
+    'id': 4,
+    "name" : "Kepler 452b",
+    "galaxy" : "Milky Way",
+    "distance" : "1150 light years"
+  }
+]
 @app.route("/")
 def hello_world():
-  return "Hello, I'm inside the route"
+  return render_template("index.html" , planets = PLANETS)
 
 if __name__ == "__main__":
-  app.run(host = "0.0.0.0", debug = True)
+  app.run(host = "0.0.0.0", debug = True )
